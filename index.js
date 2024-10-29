60c578d2b010f334f9d1b1126ba8dbe77d9feda8
Andrea Visicchio <andrea.visicchio@gmail.com>
2024-10-29 15:42:54 +0100

    ejs

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ l'opzione --save-dev permette di inidcare nel fail packge.json che questo pacche
 
 // carica il modulo express per renderlo disponibile in questo file
 const express = require('express');
+const path = require('path');
 
 // creaimo un oggetto chaimato app che recepisce tutti i metodi e le prorpiet di express
 const app = express();
@@ -47,52 +48,76 @@ le buone pratiche ci consigliano di nominare la cartella che deve contenere riso
 */
 
 // imposta la cartella per i file statici, tutti i file presenti, anche quelli che si troveranno all'itnerno di eventuali sottocartelle, verrano considerati da expressJS come risorse statiche
-app.use(  express.static('public')  );
+
+// app.use(  express.static('public')  );
+
+// ============= implementazione del codice per usare il templeting EJS ============================
+
+// il metodo set() serve a configurare le funzionalità di Express JS e node, la riga seguente indica a Express quale sarà il percorso assoluto della cartella che conterrà i file dinamici
+app.set('views', path.join(__dirname, 'views'));
+
+
+// indicazione di quale deve essere il motore di templating da  utilizzare (previa installazione del pacchetto corrisponde)
+app.set('view engine', 'ejs');
+
+// ==============================
 
 app.get('/', (richiesta, risposta) => {
 
-     risposta.sendFile(   `${__dirname}/public/home.html`   );
+     risposta.render('home.ejs')
 
 });
 
-// rotta per visualizzare la pagina prodotti.html, che verrà caricata quando l'utente digiterà sul proprio browser la url http://localhost:3000/prodotti
 app.get('/prodotti', (richiesta, risposta) => {
+     risposta.render('prodotti.ejs')
+})
 
-     risposta.sendFile(   `${__dirname}/public/prodotti.html`   );
+app.get('/i-nostri-servizi', (richiesta, risposta) => {
+     risposta.render('servizi.ejs')
+})
 
-});
-
-// rotta per visualizzare la pagina contatti.html, che verrà caricata quando l'utente digiterà sul proprio browser la url http://localhost:3000/about
-app.get('/about', (richiesta, risposta) => {
-
-     risposta.sendFile(   `${__dirname}/public/contatti.html`   );
-
-});
-
+app.get('/contatti', (richiesta, risposta) => {
+     risposta.render('contatti.ejs')
+})
 
 app.get('*', (richiesta, risposta) => {
-
-     risposta.sendFile(   `${__dirname}/public/404.html`   );
-
-});
+     risposta.render('404.ejs')
+})
 
 
 
-/* app.get('/', function(req, res) {
 
-            res.send('Ciao dalla nuova app fatta con express JS')
+// app.get('/', (richiesta, risposta) => {
+//      risposta.sendFile(   `${__dirname}/public/home.html`   );
+// });
 
-        }
-); */
+// // rotta per visualizzare la pagina prodotti.html, che verrà caricata quando l'utente digiterà sul proprio browser la url http://localhost:3000/prodotti
+// app.get('/prodotti', (richiesta, risposta) => {
+//      risposta.sendFile(   `${__dirname}/public/prodotti.html`   );
+// });
 
-/* app.get('/', (req, res) => {
 
-     res.send('Ciao dalla nuova app fatta con express JS');
-     
-}); */
+// // rotta per visulaizzare la pagina servizi.html
+// app.get('/i-nostri-servizi', (richiesta, risposta) => {
+//      risposta.sendFile(  `${__dirname}/public/servizi.html`  )
+// })
 
+
+// // rotta per visualizzare la pagina contatti.html, che verrà caricata quando l'utente digiterà sul proprio browser la url http://localhost:3000/about
+// app.get('/about', (richiesta, risposta) => {
+//      risposta.sendFile(   `${__dirname}/public/contatti.html`   );
+// });
+
+
+// app.get('*', (richiesta, risposta) => {
+//      risposta.sendFile(   `${__dirname}/public/404.html`   );
+// });
 
 
 app.listen(porta);
 
+/* 
+EJS è un template engine supportato da Node
 
+Templating => serve a definire un pattern che si ripete in più pagine e che noi possiamo facilmente modificare il suo contenuto, la modifica verrà eseguita solo una volta perchè tutti i file che rapppresenta le diverse pagine del sito si riferiranno a questo pattern
+*/
